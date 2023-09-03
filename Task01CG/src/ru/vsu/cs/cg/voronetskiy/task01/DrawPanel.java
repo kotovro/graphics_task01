a95a954b608da1267e47a2aeccee2ed61acce158
Kotvoro <kotvoro@gmail.com>
2023-09-03 16:17:46 +0300

    Created DrawPanel class and added panel for painting on window

---
+++ Task01CG/src/ru/vsu/cs/cg/voronetskiy/task01/DrawPanel.java
@@ -0,0 +1,14 @@
+package ru.vsu.cs.cg.voronetskiy.task01;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JPanel {
+    public DrawPanel() {
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
--- Task01CG/src/ru/vsu/cs/cg/voronetskiy/task01/MainWindow.java
+++ Task01CG/src/ru/vsu/cs/cg/voronetskiy/task01/MainWindow.java
@@ -1,7 +1,15 @@
 package ru.vsu.cs.cg.voronetskiy.task01;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame {
+    private final DrawPanel dp;
+    public MainWindow() throws HeadlessException {
+        dp = new DrawPanel();
+        this.add(dp);
+    }
+
+
 
 }
