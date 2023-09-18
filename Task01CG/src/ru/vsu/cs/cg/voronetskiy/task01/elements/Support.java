1340f1f997e072b4cf8411f14e4bdde2ab0a5a62
Kotvoro <kotvoro@gmail.com>
2023-09-18 03:59:40 +0300

    Crated support

--- Task01CG/src/ru/vsu/cs/cg/voronetskiy/task01/elements/Support.java
+++ Task01CG/src/ru/vsu/cs/cg/voronetskiy/task01/elements/Support.java
@@ -16,11 +16,12 @@ public class Support extends BaseElement {
         //g2d.setStroke(new BasicStroke(10));
         getScale();
         int scaledSize = (int)(size * scaleY);
-        int scaledWidth = (int)(size / 2 * scaleX);
+        int scaledWidth = (int)(size * scaleX);
         int scaledX = (int)(positionX * scaleX);
         int scaledY = (int)(positionY * scaleY);
         GeneralPath path = new GeneralPath();
         int thickness = scaledSize / 30;
+        g2d.setColor(Color.gray);
         path.moveTo(scaledX + scaledWidth / 2, scaledY);
         path.lineTo(scaledX + scaledWidth, scaledY + scaledSize);
         path.lineTo(scaledX + scaledWidth - thickness, scaledY + scaledSize);
@@ -28,7 +29,10 @@ public class Support extends BaseElement {
         path.lineTo(scaledX + thickness, scaledY + scaledSize);
         path.lineTo(scaledX, scaledY + scaledSize);
         path.moveTo(scaledX + scaledWidth / 2, scaledY);
-        g2d.setColor(Color.gray);
+        g2d.setStroke(new BasicStroke(thickness));
+        double circleHeight = (double) scaledSize / 10;
+        double circleWidth = (double) scaledWidth / 10;
+        g2d.fillOval((int) (scaledX + scaledWidth / 2 - circleWidth / 2), scaledY, (int) circleWidth, (int) circleHeight);
         g2d.fill(path);
 
     }
