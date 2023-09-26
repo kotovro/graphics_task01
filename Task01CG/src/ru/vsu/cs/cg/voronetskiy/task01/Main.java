895403891f7d488bb5df5012949ffc4ca51da3cb
Kotvoro <kotvoro@gmail.com>
2023-09-26 13:38:30 +0300

    Adds delay as field

--- Task01CG/src/ru/vsu/cs/cg/voronetskiy/task01/Main.java
+++ Task01CG/src/ru/vsu/cs/cg/voronetskiy/task01/Main.java
@@ -6,12 +6,14 @@ import java.awt.event.ActionListener;
 
 public class Main {
 
+    private static int delay = 400;
+
     public static void main(String[] args) {
         MainWindow mw = new MainWindow();
         mw.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mw.setSize(800, 600);
         mw.setVisible(true);
-        Timer timer = new Timer(400, new ActionListener() {
+        Timer timer = new Timer(delay, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 mw.repaint();
